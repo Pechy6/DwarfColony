d508d67838c18b9fa521e9d1a22736e1a82aeb60
Pechy6 <Pechyy6@gmail.com>
2026-03-26 12:16:33 +0100

    Adding first Migration data

---
+++ Migrations/20260326111422_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DwarfColony.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dwarves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Energy = table.Column<int>(type: "int", nullable: false),
+                    Hunger = table.Column<int>(type: "int", nullable: false),
+                    Thirst = table.Column<int>(type: "int", nullable: false),
+                    IsAlive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dwarves", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dwarves");
+        }
+    }
+}
