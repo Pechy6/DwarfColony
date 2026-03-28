cd014456664bf32c3981d275d2a1d5ac22eae61e
Pechy6 <Pechyy6@gmail.com>
2026-03-28 19:45:30 +0100

    Adding SeedData.cs for setup db data, modified Program.cs with Scope and adding navigation DwarfesController

---
+++ Data/SeedData.cs
@@ -0,0 +1,50 @@
+using DwarfColony.Models.Entities;
+
+namespace DwarfColony.Data;
+
+public class SeedData
+{
+    public static void Initialize(ApplicationDbContext context)
+    {
+        if (!context.Dwarves.Any())
+        {
+            context.Dwarves.Add(new Dwarf
+            { Name = "Thorin",
+                Age = 195,
+                Energy = 80,
+                Hunger = 20,
+                Thirst = 10,
+                IsAlive = true
+            });
+            context.Dwarves.Add(new Dwarf()
+            {
+                Name = "Balin",
+                Age = 178,
+                Energy = 56,
+                Hunger = 80,
+                Thirst = 90,
+                IsAlive = true
+            });
+            context.Dwarves.Add(new Dwarf()
+            {
+                Name = "Dwalin",
+                Age = 340,
+                Energy = 97,
+                Hunger = 15,
+                Thirst = 35,
+                IsAlive = true
+            });
+            context.Dwarves.Add(new Dwarf()
+            {
+                Name = "Oin",
+                Age = 250,
+                Energy = 100,
+                Hunger = 9,
+                Thirst = 86,
+                IsAlive = true
+            });
+        
+            context.SaveChanges();   
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    SeedData.Initialize(context);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
