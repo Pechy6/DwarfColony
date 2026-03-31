53d28bae95483f7749d642637823a052eb7b3c6f
Pechy6 <Pechyy6@gmail.com>
2026-03-31 12:41:29 +0200

    feat: add Id and Name for editing Dwarf

--- Models/ViewModels/EditDwarfModel.cs
+++ Models/ViewModels/EditDwarfModel.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DwarfColony.Models.ViewModels;
 
 public class EditDwarfModel
 {
-    
+    public int Id { get; set; }
+    [Required]
+    [StringLength(20, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 20 characters")]
+    public string Name { get; set; } = string.Empty;
 }
