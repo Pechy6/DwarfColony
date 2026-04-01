9a61a0ce5527b9146379b4090f24d064d9e9b380
Pechy6 <Pechyy6@gmail.com>
2026-04-01 18:34:34 +0200

    Add Job assignment migrations

---
+++ Migrations/20260401163112_Job Assignment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DwarfColony.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobAssignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Job",
+                table: "Dwarves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Job",
+                table: "Dwarves");
+        }
+    }
+}
