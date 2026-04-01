c85d405758e8b533b483fc2eaac3d40860d1b4ff
Pechy6 <Pechyy6@gmail.com>
2026-04-01 19:09:20 +0200

    feat: Add Job asignment, fix: Max name lenght to 3

--- Models/ViewModels/EditDwarfModel.cs
+++ Models/ViewModels/EditDwarfModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using DwarfColony.Models.Entities;
 
 namespace DwarfColony.Models.ViewModels;
 
@@ -6,6 +7,8 @@ public class EditDwarfModel
 {
     public int Id { get; set; }
     [Required]
-    [StringLength(20, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 20 characters")]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 4 and 20 characters")]
     public string Name { get; set; } = string.Empty;
+    
+    public DwarfJob Job { get; set; } = DwarfJob.None;
 }
