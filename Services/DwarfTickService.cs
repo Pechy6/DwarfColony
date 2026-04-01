5bd0b2021735f7c9f2d8ff3b3540960d2557c950
Pechy6 <Pechyy6@gmail.com>
2026-04-01 18:26:16 +0200

    feat: update DwarfTIckService to apply job asigment stat costs

--- Services/DwarfTickService.cs
+++ Services/DwarfTickService.cs
@@ -7,15 +7,16 @@ public class DwarfTickService
 {
     private readonly ApplicationDbContext _context;
 
-    private readonly int _energy = 1;
+    private readonly int _energy = 2;
     private readonly int _hunger = 1;
     private readonly int _thirst = 1;
+    private readonly int _jobCost = 4;
 
     public DwarfTickService(ApplicationDbContext context)
     {
         _context = context;
     }
-    
+
     public void Tick()
     {
         Stats();
@@ -24,26 +25,18 @@ public class DwarfTickService
     private void Stats()
     {
         var dwarves = _context.Dwarves.ToList();
-        
+
         foreach (var dwarf in dwarves)
         {
-            dwarf.Energy -= _energy;
-            if (dwarf.Energy <= 0)
-            {
-                dwarf.Energy = 0;
-            }
+            var jobCost = dwarf.Job != DwarfJob.None ? _jobCost : 0;
 
-            dwarf.Hunger -= _hunger;
-            if (dwarf.Hunger <= 0)
-            {
-                dwarf.Hunger = 0;
-            }
+            dwarf.Energy -= _energy + jobCost;
+            dwarf.Hunger -= _hunger + jobCost;
+            dwarf.Thirst -= _thirst + jobCost;
 
-            dwarf.Thirst -= _thirst;
-            if (dwarf.Thirst <= 0)
-            {
-                dwarf.Thirst = 0;
-            }
+            dwarf.Energy = Math.Max(0, dwarf.Energy);
+            dwarf.Hunger = Math.Max(0, dwarf.Hunger);
+            dwarf.Thirst = Math.Max(0, dwarf.Thirst);
         }
     }
 }
