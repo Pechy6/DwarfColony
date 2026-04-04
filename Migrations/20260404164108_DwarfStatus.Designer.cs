41bfea8b464961b881655ed2a54bfacb4f093e17
Pechy6 <Pechyy6@gmail.com>
2026-04-04 18:46:57 +0200

    feat: Add new migration DwarfStatus

--- Migrations/20260404164108_DwarfStatus.Designer.cs
+++ Migrations/20260404164108_DwarfStatus.Designer.cs
@@ -3,6 +3,7 @@ using DwarfColony.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DwarfColony.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260404164108_DwarfStatus")]
+    partial class DwarfStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
