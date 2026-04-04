94427db31d63b491a780c329928c1f98cf1ee29e
Pechy6 <Pechyy6@gmail.com>
2026-04-04 20:33:29 +0200

    feat: Storage entity for dwarf resources and Add Storage to AppDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,4 +6,5 @@ namespace DwarfColony.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Dwarf> Dwarves { get; set; }
+    public DbSet<Storage> Storages { get; set; }
 }
---
+++ Models/Entities/Storage.cs
@@ -0,0 +1,19 @@
+namespace DwarfColony.Models.Entities;
+
+public class Storage
+{
+    public int Id { get; set; }
+    
+    // Essentials needs
+    public int RawFood { get; set; }
+    public int Food { get; set; }
+    public int Water { get; set; }
+    
+    // Resources from mining
+    public int Stone { get; set; }
+    public int IronCore { get; set; }
+    public int Coal { get; set; }
+    
+    // Resources from woodcutting
+    public int Wood { get; set; }
+}
