83f230e489174539fa316eeec1327f6e15621ee4
Pechy6 <Pechyy6@gmail.com>
2026-04-06 13:27:16 +0200

    chore: add connection Dwarf tick service and Resource production service to Tick Service

--- Services/TickService.cs
+++ Services/TickService.cs
@@ -1,6 +1,13 @@
 namespace DwarfColony.Services;
 
-public class TickService
+public class TickService(ResourceProductionService resourceProductionService, DwarfTickService dwarfTickService)
 {
-    
+    private readonly ResourceProductionService _resourceProductionService = resourceProductionService;
+    private readonly DwarfTickService _dwarfTickService = dwarfTickService;
+
+    public void Tick()
+    {
+        _resourceProductionService.ProduceManager();
+        _dwarfTickService.Tick();
+    }
 }
