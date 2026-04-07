27d885601778ab1b941068e8a885b02ec824cbfa
Pechy6 <Pechyy6@gmail.com>
2026-04-07 20:49:21 +0200

    Add comment to Navbar stats view component

--- ViewComponents/NavbarStatsViewComponent.cs
+++ ViewComponents/NavbarStatsViewComponent.cs
@@ -8,6 +8,13 @@ public class NavbarStatsViewComponent(ApplicationDbContext context) : ViewCompon
 {
     private readonly ApplicationDbContext _context = context;
 
+    /// <summary>
+    /// Retrieves the current state of resources and the number of dwarves
+    /// from the database, aggregates the relevant information, and renders a view
+    /// with a model containing these details.
+    /// </summary>
+    /// <returns>An instance of <see cref="IViewComponentResult"/> containing
+    /// the aggregated resource data and dwarf count for display in the view.</returns>
     public IViewComponentResult Invoke()
     {
         var storage = _context.Storages.FirstOrDefault();
