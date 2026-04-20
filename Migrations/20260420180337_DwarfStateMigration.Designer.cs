5c0162477bfb28061a8558364f382026c83b5e0f
Pechy6 <Pechyy6@gmail.com>
2026-04-20 20:09:17 +0200

    feat: Dwarf status (Idle, working, sleeping..) migration

--- Migrations/20260420180337_DwarfStateMigration.Designer.cs
+++ Migrations/20260420180337_DwarfStateMigration.Designer.cs
@@ -3,6 +3,7 @@ using DwarfColony.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DwarfColony.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260420180337_DwarfStateMigration")]
+    partial class DwarfStateMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
