47e93eceb2877c20c7bb6f0bd6f7d9713b3a5ffb
Pechy6 <Pechyy6@gmail.com>
2026-04-25 16:22:29 +0200

    Add dwarf reference into Area

--- Models/Entities/Area.cs
+++ Models/Entities/Area.cs
@@ -15,15 +15,16 @@ public class Area
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    
+
     public AreaType Type { get; set; }
-    
+
     public int MaxWorkers { get; set; }
-    
+
     public int DistanceFromBase { get; set; }
 
     public bool IsUnlocked { get; set; } = false;
     public bool CanRest { get; set; } = false;
-    
+
     public ICollection<AreaResource> Resources { get; set; } = new List<AreaResource>();
+    public ICollection<Dwarf> Dwarves { get; set; } = new List<Dwarf>();
 }
