b866c9e91ee1b1bdfc7a1de42f0467382730cce5
Pechy6 <Pechyy6@gmail.com>
2026-04-25 22:03:14 +0200

    feat(SeedData): Add new data for creating areas and set dwarfs to location. feat(migrations) add migrations with creating table Areas and Area resource

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -6,50 +6,40 @@ public class SeedData
 {
     public static void Initialize(ApplicationDbContext context)
     {
-        if (!context.Dwarves.Any())
+        // Areas
+        if (!context.Areas.Any())
         {
-            context.Dwarves.Add(new Dwarf
-            { Name = "Thorin",
-                Age = 195,
-                Energy = 100,
-                Hunger = 100,
-                Thirst = 100,
-                IsAlive = true
-            });
-            context.Dwarves.Add(new Dwarf()
+            context.Areas.Add(new Area
             {
-                Name = "Balin",
-                Age = 178,
-                Energy = 100,
-                Hunger = 100,
-                Thirst = 100,
-                IsAlive = true
+                Name = "Dwarf basement",
+                Description = "The basement of the dwarves",
+                Type = AreaType.Base,
+                DistanceFromBase = 0,
+                MaxWorkers = 999,
+                IsUnlocked = true,
+                CanRest = true
             });
-            context.Dwarves.Add(new Dwarf()
+
+            context.Areas.Add(new Area
             {
-                Name = "Dwalin",
-                Age = 340,
-                Energy = 100,
-                Hunger = 100,
-                Thirst = 100,
-                IsAlive = true
+                Name = "North forest",
+                Description =
+                    "The Northern Forest is a dense woodland filled with wildlife," +
+                    " trees, and scattered stones. A small stream with clean water flows" +
+                    " through the area, making it a safe and resource-rich location for beginners.",
+                Type = AreaType.Forest,
+                DistanceFromBase = 2,
+                MaxWorkers = 10,
+                IsUnlocked = true,
+                CanRest = false
             });
-            context.Dwarves.Add(new Dwarf()
-            {
-                Name = "Oin",
-                Age = 250,
-                Energy = 100,
-                Hunger = 100,
-                Thirst = 100,
-                IsAlive = true
-            });
-        
-            context.SaveChanges();   
+            context.SaveChanges();
         }
-        
+
+        // Storage
         if (!context.Storages.Any())
         {
-            context.Storages.Add(new Storage()
+            context.Storages.Add(new Storage
             {
                 RawFood = 100,
                 Food = 50,
@@ -59,7 +49,59 @@ public class SeedData
                 IronCore = 20,
                 Wood = 100
             });
-            context.SaveChanges();  
+            context.SaveChanges();
+        }
+
+        // Dwarves
+        if (!context.Dwarves.Any())
+        {
+            var homeArea = context.Areas.FirstOrDefault(a => a.Type == AreaType.Base);
+
+            if (homeArea != null)
+            {
+                context.Dwarves.Add(new Dwarf
+                {
+                    Name = "Thorin",
+                    Age = 195,
+                    Energy = 100,
+                    Hunger = 100,
+                    Thirst = 100,
+                    IsAlive = true,
+                    CurrentAreaId = homeArea.Id
+                });
+                context.Dwarves.Add(new Dwarf
+                {
+                    Name = "Balin",
+                    Age = 178,
+                    Energy = 100,
+                    Hunger = 100,
+                    Thirst = 100,
+                    IsAlive = true,
+                    CurrentAreaId = homeArea.Id
+                });
+                context.Dwarves.Add(new Dwarf
+                {
+                    Name = "Dwalin",
+                    Age = 340,
+                    Energy = 100,
+                    Hunger = 100,
+                    Thirst = 100,
+                    IsAlive = true,
+                    CurrentAreaId = homeArea.Id
+                });
+                context.Dwarves.Add(new Dwarf
+                {
+                    Name = "Oin",
+                    Age = 250,
+                    Energy = 100,
+                    Hunger = 100,
+                    Thirst = 100,
+                    IsAlive = true,
+                    CurrentAreaId = homeArea.Id
+                });
+            }
+
+            context.SaveChanges();
         }
     }
 }
--- Migrations/20260425144745_AreaAndAreaResourceMigration.Designer.cs
+++ Migrations/20260425144745_AreaAndAreaResourceMigration.Designer.cs
@@ -3,6 +3,7 @@ using DwarfColony.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DwarfColony.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260425144745_AreaAndAreaResourceMigration")]
+    partial class AreaAndAreaResourceMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
