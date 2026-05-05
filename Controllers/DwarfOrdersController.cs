4f58fec2503641eb03fc55f51d85fc1d229e7ec4
Pechy6 <Pechyy6@gmail.com>
2026-05-05 21:07:17 +0200

    feat(controller): add post action for start travel. feat(SAPostVM): add view model for validation just only dwarfid and areaid. refactor(view): Add summit and change view for better ui

--- Controllers/DwarfOrdersController.cs
+++ Controllers/DwarfOrdersController.cs
@@ -64,8 +64,39 @@ public class DwarfOrdersController(
                 }).
                 ToList(),
             Distance = dwarf.TravelRemainingTicks,
-            Description = area.FirstOrDefault(a => a.Id == dwarf.TargetAreaId)?.Description ?? "No description available"
+            Description = area.FirstOrDefault(a => a.Id == dwarf.TargetAreaId)?.
+                Description ?? "No description available"
         };
         return View(viewModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult SetArea(int dwarfId, int targetAreaId, SetAreaPostViewModel viewModel)
+    {
+        if (dwarfId != viewModel.DwarfId || targetAreaId != viewModel.TargetAreaId)
+        {
+            return NotFound();
+        }
+
+        var dwarf = _context.Dwarves.Find(dwarfId);
+        if (dwarf is null)
+        {
+            return NotFound();
+        }
+
+        var targetArea = _context.Areas.Find(targetAreaId);
+        if (targetArea is null)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            _moveToArea.StartTravel(dwarf, targetArea);
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ Models/ViewModels/SetAreaPostViewModel.cs
@@ -0,0 +1,7 @@
+namespace DwarfColony.Models.ViewModels;
+
+public class SetAreaPostViewModel
+{
+    public int DwarfId { get; set; }
+    public int TargetAreaId { get; set; }
+}
